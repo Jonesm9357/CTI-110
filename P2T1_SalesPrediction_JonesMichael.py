4c42e0cab1d5a75a782e53ab5c7550c3c5c55228
Jonesm9357 <36610385+Jonesm9357@users.noreply.github.com>
2018-02-18 20:00:09 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_JonesMichael.py
@@ -0,0 +1,20 @@
+# CTI-110
+
+# P2T1 - Sales Prediction
+
+# Michael Jones
+
+# 2/18/18
+
+# Get the projected total sales.
+
+total_sales = float(input('Enter the projected sales:'))
+
+#Calculate the profit as 23 percent of total sales.
+
+profit  = total_sales * 0.23
+
+# Display the profit.
+
+print('The profit is $',profit)
+
